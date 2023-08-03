0159509e5947a877261342dbb93d5e46e8276482
teepheh-git <teepheh@yahoo.com>
2023-08-03 11:46:14 +0100

    update

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -28,6 +28,6 @@ export default [
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
-    external: ["react"],
+    external: ["react", "web3", "magic-sdk", "ethers", "@gelatonetwork/relay-sdk", "axios"],
   },
 ];
